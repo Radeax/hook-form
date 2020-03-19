5f8ca2fa4f70c327c7046524f9b081d74fe5d70e
Radeax <joflip@gmail.com>
2020-03-19 17:03:55 -0400

    Converted to Functional Components

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -1,43 +1,31 @@
 import React, { useState, Component } from 'react';
 
-class FormRow extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    onChangeHandler = (event) => {
-        this.props.changeState(event.target.value);
-
-    }
-
-    render() {
-        return (
-            <input value={this.props.state} onChange={e => this.onChangeHandler(e)} />
-        )
-    }
-}
-
-// const FormRow = (props) => {
-//     const onChangeHandler = () => {
-//         console.log(props.state);
-
+// class FormRow extends Component {
+//     constructor(props) {
+//         super(props);
 //     }
 
-//     return (
-//         <input value={props.state} onChange={onChangeHandler} />
-//     )
+//     onChangeHandler = (event) => {
+//         this.props.changeState(event.target.value);
+//     }
 
-
-
+//     render() {
+//         return (
+//             <input value={this.props.state} onChange={e => this.onChangeHandler(e)} />
+//         )
+//     }
 // }
 
+const FormRow = (props) => {
+    const [state, setState] = useState('');
 
-// const onChangeHandler = event => {
-//     setState({
-//         ...state,
-//         value: event.target.value
-//     })
-//     console.log(state)
-// }
+    const onChangeHandler = e => {
+        props.setState(e.target.value)
+    }
+
+    return (
+        <input value={props.state} onChange={onChangeHandler} />
+    );
+}
 
 export default FormRow;
--- src/components/Table.js
+++ src/components/Table.js
@@ -2,42 +2,47 @@ import React, { useState, Component } from 'react';
 import FormRow from './FormRow';
 import Display from './Display';
 
-class Table extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ''
-        }
-    }
+// class Table extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             value: 'fff'
+//         }
+//     }
 
-    updateInput = (value) => {
-        this.setState({
-            value: value
+//     updateInput = (value) => {
+//         this.setState({
+//             value: value
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <FormRow state={this.state.value} changeState={this.updateInput} />
+//                 <Display state={this.state.value} />
+//             </div>
+//         )
+//     }
+// }
+
+const Table = props => {
+    const [state, setState] = useState({
+        value: 'Default Value'
+    });
+
+    const onChangeHandler = e => {
+        setState({
+            value: e
         })
     }
 
-    render() {
-        return (
-            <div>
-                <FormRow state={this.state.value} changeState={this.updateInput} />
-                <Display state={this.state.value} />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <FormRow state={state.value} setState={onChangeHandler} />
+            <Display state={state.value} />
+        </div>
+    );
 }
-
-
-// const Table = (props) => {
-//     const [state, setState] = useState('');
-
-
-
-//     return (
-//         <div>
-//             <FormRow state={state} setState={onChangeHandler} />
-//             <Display val={state} output={state} />
-//         </div>
-//     );
-// }
 
 export default Table
